cc97be88865de224ab9aad60bbaa355070eec7ba
java-fanatic <chilwantsameer@gmail.com>
2025-06-23 21:30:24 +0530

    Arithmetic added

---
+++ src/test/java/Java_Assignment/Comparision_Logical_Operation.java
@@ -0,0 +1,17 @@
+package Java_Assignment;
+
+public class Comparision_Logical_Operation {
+    public static void main(String[] args) {
+        // initializing variables
+        int a = 10, b = 5;
+
+        System.out.println(a==b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println((a > b) && (a > 0));
+        System.out.println((a < b) || (a > 0));
+        System.out.println(!(a > b));
+
+}
+    }
+
