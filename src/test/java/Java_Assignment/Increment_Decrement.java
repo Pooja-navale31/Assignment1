32af4376191085e4da9277e67509a5244f25545d
java-fanatic <chilwantsameer@gmail.com>
2025-06-23 21:41:45 +0530

    Arithmetic added

---
+++ src/test/java/Java_Assignment/Increment_Decrement.java
@@ -0,0 +1,15 @@
+package Java_Assignment;
+
+public class Increment_Decrement {
+    public static void main(String[] args) {
+
+
+        int a = 5;
+        System.out.println(++a);
+        System.out.println(a++);
+        int b=6;
+        System.out.println(--b);
+        System.out.println(b--);
+
+    }
+}
---
+++ src/test/java/Java_Assignment/TypeCasting.java
@@ -0,0 +1,12 @@
+package Java_Assignment;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        int b = 10;
+        int a = b; //  Widening -> Implicit Casting
+        int a1 = (int)b; // Widening -> Explicit Casting
+        }
+    }
+
+
