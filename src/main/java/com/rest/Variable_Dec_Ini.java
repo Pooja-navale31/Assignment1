fc2cb80dface8f33b9d1381776553540102e47db
java-fanatic <chilwantsameer@gmail.com>
2025-06-23 21:13:57 +0530

    Assignment adding

---
+++ src/main/java/com/rest/Variable_Dec_Ini.java
@@ -0,0 +1,20 @@
+package com.rest;
+
+public class Variable_Dec_Ini {
+    public static void main(String[] args) {
+        int k=5;
+        double d=10;
+
+        boolean b=true;
+        String string="pooja";
+
+
+            System.out.println("Integer = " + k);
+            System.out.println("double = " + d);
+            System.out.println("boolean default value = " + b);
+            System.out.println("String default value = " + string);
+        }
+    }
+
+
+
